3b7ffdcd9d7a2bf262d20207cb18f1b287867cf3
saravanan@91mobiles.com <saravanan291>
2026-04-17 21:29:15 +0530

    ss

--- scripts/split_phones.py
+++ scripts/split_phones.py
@@ -5,8 +5,8 @@ Usage: python3 split_phones.py
 """
 
 MASTER_FILE = "data/masterPhone..txt"
-BATCH_SIZE = 295
-NUM_BATCHES = 21
+BATCH_SIZE = 300
+NUM_BATCHES = 20
 
 def split_phones():
     # Read all phone numbers
