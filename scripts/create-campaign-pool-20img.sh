b1c49e89e742bbd3fc401db92eddda78b96f3adb
vinoddangra <vinod.dangra@gmail.com>
2026-04-25 18:53:14 +0530

    25 april

--- scripts/create-campaign-pool-20img.sh
+++ scripts/create-campaign-pool-20img.sh
@@ -18,7 +18,7 @@
 set -e
 
 S3_BUCKET="greendotball-bot-data"
-POOL_PREFIX="green_ball_image_24apr/"
+POOL_PREFIX="green_ball_image_25apr/"
 REGION="ap-south-1"
 PHONES_FILE="data/phones.txt"
 PHONES_PER_JOB=9
